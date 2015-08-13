eec2c619f7300b3dd1d1a5f80f829679e22c58d3
Jonas Karlman <jonas@kwiboo.se>
2015-08-13 22:23:26 +0200

    Upgrade RPi firmware to c25cc51

--- packages/graphics/bcm2835-driver/package.mk
+++ packages/graphics/bcm2835-driver/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="bcm2835-driver"
-PKG_VERSION="47d51d6"
+PKG_VERSION="c25cc51"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="nonfree"
