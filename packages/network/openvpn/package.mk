1a1d2b86b2a1c6e12ca3c71066a54f8b3c126cbc
Jonas Karlman <jonas@kwiboo.se>
2015-08-23 12:28:54 +0200

    Upgrade OpenVPN to 2.3.8

--- packages/network/openvpn/package.mk
+++ packages/network/openvpn/package.mk
@@ -18,7 +18,7 @@
 ################################################################################
 
 PKG_NAME="openvpn"
-PKG_VERSION="2.3.7"
+PKG_VERSION="2.3.8"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
