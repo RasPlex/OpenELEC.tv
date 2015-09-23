f570ba6f4a04030c7a446f49a15c325edff3ce1a
Jonas Karlman <jonas@kwiboo.se>
2015-09-23 21:47:17 +0200

    Upgrade RPi firmware to 960832a

--- packages/graphics/bcm2835-driver/package.mk
+++ packages/graphics/bcm2835-driver/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="bcm2835-driver"
-PKG_VERSION="bbf5495"
+PKG_VERSION="960832a"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="nonfree"
