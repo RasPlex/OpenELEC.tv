d226f6518aea6478bbb25a8a94e3124822b6db11
Jonas Karlman <jonas@kwiboo.se>
2016-02-20 15:36:38 +0100

    Upgrade RPi firmware to 565197e

--- packages/tools/bcm2835-bootloader/package.mk
+++ packages/tools/bcm2835-bootloader/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="bcm2835-bootloader"
-PKG_VERSION="fbe3633"
+PKG_VERSION="565197e"
 PKG_REV="1"
 PKG_ARCH="arm"
 PKG_LICENSE="nonfree"
