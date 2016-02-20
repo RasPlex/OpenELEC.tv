ebc5a12f519a6b15497711b47e9f32a5fda7ef68
Jonas Karlman <jonas@kwiboo.se>
2016-02-20 15:35:21 +0100

    Upgrade OpenVPN to 2.3.10

--- packages/network/openvpn/package.mk
+++ packages/network/openvpn/package.mk
@@ -18,7 +18,7 @@
 ################################################################################
 
 PKG_NAME="openvpn"
-PKG_VERSION="2.3.9"
+PKG_VERSION="2.3.10"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
